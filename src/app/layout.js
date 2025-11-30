3632e0e705a232e4129de751268cd38d649b064f
rodrigofaria <bragafaria@gmail.com>
2025-11-30 02:41:51 -0300

    setting PostHog

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,16 @@
 // app/layout.tsx (final version)
 import "./globals.css";
 import SupabaseListener from "@/app/lib/supabase/SupabaseListerner";
+import { PostHogProvider } from "@/app/providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SupabaseListener />
-        {children}
+        <PostHogProvider>
+          <SupabaseListener />
+          {children}
+        </PostHogProvider>
       </body>
     </html>
   );
