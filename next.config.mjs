37f0a9c945bf3e88a77636a1b9dad9d61531768d
rodrigofaria <bragafaria@gmail.com>
2025-11-30 02:28:23 -0300

    rollback nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        // Root of subdomain -> blog home
-        source: "/",
-        destination: "/blog",
-        has: [
-          {
-            type: "host",
-            value: "blog.csmdynamics.com",
-          },
-        ],
-      },
-      {
-        // All other paths on subdomain -> /blog/path
-        source: "/:path*",
-        destination: "/blog/:path*",
-        has: [
-          {
-            type: "host",
-            value: "blog.csmdynamics.com",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
