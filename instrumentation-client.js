3f082ee446809e311021caadd4fbf635cf249ef9
rodrigofaria <bragafaria@gmail.com>
2025-12-07 00:58:53 -0300

    posthog

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -2,16 +2,5 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  person_profiles: "identified_only",
-  autocapture: true,
-  capture_pageview: true,
-  capture_pageleave: true,
-  sanitize_properties: (props) => {
-    delete props.password;
-    return props;
-  },
-  session_recording: {
-    maskAllInputs: false,
-    maskAllText: false,
-  },
+  defaults: "2025-11-30",
 });
